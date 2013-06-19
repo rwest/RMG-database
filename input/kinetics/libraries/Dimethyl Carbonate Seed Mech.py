00706e69ae154914341979a06e3b15f2ff35c2fa
Richard West <r.west@neu.edu>
2013-06-19 13:07:55 -0400

    Corrected the Dimethyl Carbonate decomposition rate.
    * The A factor was off by a factor of 10 (typo?).
    * The Ea was in the wrong units (they reported Ea/R in Kelvin)
    * The source was not specified.
    
    NB > this rate is at 0.5 atm.

--- input/kinetics/libraries/Dimethyl Carbonate Seed Mech.py
+++ input/kinetics/libraries/Dimethyl Carbonate Seed Mech.py
@@ -2,9 +2,11 @@
 # encoding: utf-8
 
 name = "Dimethyl Carbonate"
-shortDesc = u""
+shortDesc = u"Peukert 2013 doi:10.1021/jp312643k"
 longDesc = u"""
-
+Peukert, S. L.; Sivaramakrishnan, R.; Michael, J. V. “High Temperature Shock Tube and Theoretical
+Studies on the Thermal Decomposition of Dimethyl Carbonate and Its Bimolecular Reactions with H and
+D-Atoms.” J. Phys. Chem. A 2013, 117, 3718–3728 [doi:10.1021/jp312643k].
 """
 recommended = False
 
@@ -37,19 +39,22 @@ CO2
 """,
 	degeneracy = 1,
 	kinetics = Arrhenius(
-		A = (4.29e83, 's^-1'),
+		A = (4.29e82, 's^-1'),
 		n = -19.437,
-		Ea = (55.7, 'kcal/mol'),
+		Ea = (110.687, 'kcal/mol'), # the molar gas constant * 55 700 kelvin = 110.68727 kcal / mol
 		T0 = (1, 'K'),
 	),
 	reference = None,
 	referenceType = "",
-	shortDesc = u"""""",
-	longDesc = 
-u"""
-
-""",
+	shortDesc = u"Peukert 2013 doi:10.1021/jp312643k"
+    longDesc = u"""Reaction 1 in the following paper. Rate taken from Table 3.
+*From the text this appears to be at 0.5 atm from a master equation analysis!*
+[1] Peukert, S. L.; Sivaramakrishnan, R.; Michael, J. V. “High Temperature Shock Tube and Theoretical
+Studies on the Thermal Decomposition of Dimethyl Carbonate and Its Bimolecular Reactions with H and
+D-Atoms.” J. Phys. Chem. A 2013, 117, 3718–3728 [doi:10.1021/jp312643k].
+    """,
 	history = [
-			("Mon May 6 11:13:30 2013","Fariba Seyedzadeh <seyedzadehkhanshan.f@husky.neu.edu>","action","""Fariba Seyedzadeh <seyedzadehkhanshan.f@husky.neu.edu> imported this entry from the Huynh model."""),
+			("Mon May 6 11:13:30 2013","Fariba Seyedzadeh <seyedzadehkhanshan.f@husky.neu.edu>","action","""Fariba Seyedzadeh <seyedzadehkhanshan.f@husky.neu.edu> imported this entry from the Peukert model."""),
+            ("Wed June 19 2013","Richard West <r.west@neu.edu>","action","""Richard West corrected this entry from doi:10.1021/jp312643k."""),
 		],
 	)
