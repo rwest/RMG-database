ee3c85b1ca0efd67b438a3cc54befff3ed63e506
Belinda Slakman <slakman.b@husky.neu.edu>
2014-11-26 14:56:01 -0800

    Changes to silicon hydride thermo.
    Made group definitions more general; added more thermodynamic data.

--- input/thermo/groups/group.py
+++ input/thermo/groups/group.py
@@ -41401,7 +41401,7 @@ entry(
     group = 
 """
 1 * Sis u0 p0 c0 {2,S}
-2   Sis u0 p1 c0 {1,S}
+2   Sis u0 p[1,2] c0 {1,S}
 """,
     thermo = ThermoData(
         Tdata = ([300, 400, 500, 600, 800, 1000, 1500],'K'),
@@ -41590,9 +41590,7 @@ entry(
     label = "si",
     group = 
 """
-1 * Si u0 p1 c0 {2,S} {3,S}
-2   R u0 px c0 {1,S}
-3   R u0 px c0 {1,S}
+1 * Si u0 p[1,2] c0 
 """,
     thermo = ThermoData(
         Tdata = ([300, 400, 500, 600, 800, 1000, 1500],'K'),
@@ -43738,7 +43736,7 @@ entry(
 """
 1 * Sis u0 p0 c0 {2,S} {3,S}
 2   Sis u0 p0 c0 {1,S}
-3   Si  u0 p1 c0 {1,S}
+3   Si  u0 p[1,2] c0 {1,S}
 """,
     thermo = ThermoData(
         Tdata = ([300, 400, 500, 600, 800, 1000, 1500],'K'),
