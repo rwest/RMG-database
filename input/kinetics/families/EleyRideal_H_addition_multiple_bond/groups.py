e79ad62f5a721a5e7c7052bde4ff200de5f93b4c
Emily Mazeau <mazeau.e@husky.neu.edu>
2019-02-14 15:58:54 -0500

    made a nice tree

--- input/kinetics/families/EleyRideal_H_addition_multiple_bond/groups.py
+++ input/kinetics/families/EleyRideal_H_addition_multiple_bond/groups.py
@@ -35,8 +35,8 @@ entry(
     label = "Adsorbate1",
     group =
 """
-1 *1 H u0 p0 {2,S}
-2 *2 Xo u0 {1,S}
+1 *1 Xo u0 p0 {2,S}
+2 *2 R  u0 {1,S}
 """,
     kinetics = None,
 )
@@ -46,17 +46,77 @@ entry(
     label="Gas",
     group =
 """
-1 *3 R u0 {2,[D,T,Q]}
-2 *4 R u0 {1,[D,T,Q]}
+1 *3 R!H ux {2,[D,T,Q]}
+2 *4 R!H ux {1,[D,T,Q]}
 """,
     kinetics = None,
 )
 
+entry(
+    index = 3,
+    label="*H",
+    group =
+"""
+1 *1 Xo u0 {2,S}
+2 *2 H  u0 {1,S}
+""",
+    kinetics = None,
+)
+
+entry(
+    index = 4,
+    label="R=R",
+    group =
+"""
+1 *3 R!H ux {2,D}
+2 *4 R!H ux {1,D}
+""",
+    kinetics = None,
+)
+
+entry(
+    index = 5,
+    label="R#R",
+    group =
+"""
+1 *3 R!H ux {2,T}
+2 *4 R!H ux {1,T}
+""",
+    kinetics = None,
+)
+
+entry(
+    index = 6,
+    label="C=C",
+    group =
+"""
+1 *3 C u0 {2,D}
+2 *4 C u0 {1,D}
+""",
+    kinetics = None,
+)
+
+entry(
+    index = 7,
+    label="C#C",
+    group =
+"""
+1 *3 C u0 {2,T}
+2 *4 C u0 {1,T}
+""",
+    kinetics = None,
+)
+
+
 tree(
 """
 L1: Adsorbate1
-
+    L2: *H
 L1: Gas
+    L2: R=R
+        L3: C=C
+    L2: R#R
+        L3: C#C
 """
 )
 
