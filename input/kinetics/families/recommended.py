b95ff7243dd896a1da69ae51105d91516c36f560
Emily Mazeau <mazeau.e@husky.neu.edu>
2019-02-14 16:50:30 -0500

    got rid of deleted duplicate family in recommended.py

--- input/kinetics/families/recommended.py
+++ input/kinetics/families/recommended.py
@@ -80,7 +80,6 @@ surface = {
     'Surface_Dissociation',
     'Surface_Abstraction',
     'EleyRideal_H_addition_multiple_bond',
-    'EleyRideal_H_deletion_multiple_bond',
 }
 
 # Surface chemistry families that are under development and not yet working well.
