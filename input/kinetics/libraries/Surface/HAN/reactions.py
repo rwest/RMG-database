f88f4f72ba77bca373039a930836fe344f9d902a
Richard West <r.west@northeastern.edu>
2020-05-22 16:29:31 -0400

    Removing redundant stuff from HAN reaction library.
    These were in the surface N2 library, or duplicated.

--- input/kinetics/libraries/Surface/HAN/reactions.py
+++ input/kinetics/libraries/Surface/HAN/reactions.py
@@ -8,71 +8,6 @@ Reactions pertinent to the decomposition of HAN
 """
 
 entry(
-    index = 2,
-    label = "NH3 + X <=> NH3_X",
-    kinetics = StickingCoefficient(
-        A = 0.011,
-        n = 0,
-        Ea=(0, 'kJ/mol'),
-        Tmin = (200, 'K'),
-        Tmax = (3000, 'K'),
-    ),
-    shortDesc = u"""NH3 Surface Adsorption vdW""",
-    longDesc = u"""
-    Micro-kinetic modeling of NH3 decomposition on Ni and its application to solid oxide fuel cells
-    Deutschmann et al
-    doi: 10.1016/j.ces.2011.07.007
-
-    This is R2
-    metal = 'Ni'
-    """
-)
-
-entry(
-    index = 3,
-    label = "N2 + X + X <=> N_X + N_X",
-    kinetics = StickingCoefficient(
-        A = 1.0e-6,
-        n = 0,
-        Ea=(0, 'kJ/mol'),
-        Tmin = (200, 'K'),
-        Tmax = (3000, 'K'),
-    ),
-    shortDesc = u"""N2 Adsorption Dissociative""",
-    longDesc = u"""
-    Micro-kinetic modeling of NH3 decomposition on Ni and its application to solid oxide fuel cells
-    Deutschmann et al
-    doi: 10.1016/j.ces.2011.07.007
-
-    This is R3
-    metal = 'Ni'
-    """
-)
-
-entry(
-    index = 51,
-    label = "NO_X + X <=> N_X + O_X",
-    kinetics = SurfaceArrhenius(
-        A = (5e20, 'cm^2/(mol*s)'),
-        n = 0,
-        Ea=(107.8, 'kJ/mol'),
-        Tmin = (200, 'K'),
-        Tmax = (3000, 'K'),
-    ),
-    shortDesc = u"""NO Dissociation""",
-    longDesc = u"""
-    Detailed surface reaction mechanism in a three-way catalyst
-    Chatterjee, Deutschmann, Warnatz et al.
-    doi: 10.1039/b101968f
-
-    is supposedly only valid for a Pt/Rh ratio of 75%/25%
-
-    This is R51
-    metal = 'Pt'
-    """
-)
-
-entry(
     index = 1,
     label = "NH4NO3 + X + X <=> N2O_X + H2O_X + H2O",
     kinetics = StickingCoefficient(
@@ -110,19 +45,3 @@ entry(
     """
 )
 
-
-entry( 
-    index = 2,
-    label = "HAN_X + X <=> HO_X + NH2O_X + HONO",
-    kinetics = SurfaceArrhenius(
-        A = (5e20, 'cm^2/(mol*s)'),
-        n = 0,
-        Ea=(8.07, 'kcal/mol'),
-        Tmin = (200, 'K'),
-        Tmax = (3000, 'K'),
-    ),
-    shortDesc = u"""HONO elimination from HAN on Pd""",
-    longDesc = u"""
-    The "HONO elimaniton" pathway which is fastest on Pd(111) from Banerjee
-    """
-)
