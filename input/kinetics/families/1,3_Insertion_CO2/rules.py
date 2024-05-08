c625a998370ad6153928484bfdea9401f998d002
Nora Khalil <khalil.nor@northeastern.edu>
2024-05-08 15:54:34 -0400

    trying to autogenerate 1,3_Insertion_CO2, try 1

--- input/kinetics/families/1,3_Insertion_CO2/rules.py
+++ input/kinetics/families/1,3_Insertion_CO2/rules.py
@@ -11,18 +11,18 @@ The dictionary defines CO2 in two ways, allowing the R-R' to insert either way
 around. However, there are only rates for one of these ways. The other is
 presumably matching the top level node.
 """
-entry(
-    index = 571,
-    label = "CO2_CS2;RR'",
-    kinetics = ArrheniusEP(
-        A = (100000, 'cm^3/(mol*s)'),
-        n = 2,
-        alpha = 0,
-        E0 = (75, 'kcal/mol'),
-        Tmin = (300, 'K'),
-        Tmax = (1500, 'K'),
-    ),
-    rank = 0,
-    shortDesc = u"""Default""",
-)
+# entry(
+#     index = 571,
+#     label = "CO2_CS2;RR'",
+#     kinetics = ArrheniusEP(
+#         A = (100000, 'cm^3/(mol*s)'),
+#         n = 2,
+#         alpha = 0,
+#         E0 = (75, 'kcal/mol'),
+#         Tmin = (300, 'K'),
+#         Tmax = (1500, 'K'),
+#     ),
+#     rank = 0,
+#     shortDesc = u"""Default""",
+# )
 
