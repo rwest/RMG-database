fa560d5b2f424d8a545b955232bfdad88bee3ca1
Nora Khalil <khalil.nor@northeastern.edu>
2024-05-13 14:38:31 -0400

    trying to autogenerate Intra_R_Add_Endocyclic

--- input/kinetics/families/Intra_R_Add_Endocyclic/rules.py
+++ input/kinetics/families/Intra_R_Add_Endocyclic/rules.py
@@ -6,17 +6,17 @@ shortDesc = u""
 longDesc = u"""
 
 """
-entry(
-    index = 809,
-    label = "Rn;multiplebond_intra;radadd_intra",
-    kinetics = ArrheniusEP(
-        A = (1e+09, 's^-1'),
-        n = 0.19,
-        alpha = 0,
-        E0 = (20, 'kcal/mol'),
-        Tmin = (300, 'K'),
-        Tmax = (1500, 'K'),
-    ),
-    rank = 10,
-)
+# entry(
+#     index = 809,
+#     label = "Rn;multiplebond_intra;radadd_intra",
+#     kinetics = ArrheniusEP(
+#         A = (1e+09, 's^-1'),
+#         n = 0.19,
+#         alpha = 0,
+#         E0 = (20, 'kcal/mol'),
+#         Tmin = (300, 'K'),
+#         Tmax = (1500, 'K'),
+#     ),
+#     rank = 10,
+# )
 
