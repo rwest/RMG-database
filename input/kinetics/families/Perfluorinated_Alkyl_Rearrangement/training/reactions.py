1f8469e37380275855ea61228f0576944485668a
Nora Khalil <khalil.nor@northeastern.edu>
2024-08-22 10:46:50 -0400

    First attempt at creating Perfluorinated_Alkyl_Rearrangement new kinetic family

---
+++ input/kinetics/families/Perfluorinated_Alkyl_Rearrangement/training/reactions.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "Perfluorinated_Alkyl_Rearrangement/training"
+shortDesc = "Reaction kinetics used to generate rate rules"
+longDesc = """
+Put kinetic parameters for specific reactions in this file to use as a
+training set for generating rate rules to populate this kinetics family.
+"""
