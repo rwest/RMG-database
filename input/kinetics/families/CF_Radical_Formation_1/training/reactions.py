1d2e36c2826d2a64401d9e79ae6bb179dfc8deac
Nora Khalil <khalil.nor@northeastern.edu>
2024-08-23 10:51:06 -0400

    syntax error in CF_Radical_Formation_1 training

--- input/kinetics/families/CF_Radical_Formation_1/training/reactions.py
+++ input/kinetics/families/CF_Radical_Formation_1/training/reactions.py
@@ -6,7 +6,8 @@ shortDesc = "Reaction kinetics used to generate rate rules"
 longDesc = """
 Put kinetic parameters for specific reactions in this file to use as a
 training set for generating rate rules to populate this kinetics family.
-"""entry(
+"""
+entry(
     index = 1,
     label = "CHF2 <=> FH + CF",
     degeneracy = 2.0,
