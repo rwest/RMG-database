39806fa0070a20b0dafdcc7119a536e286bfe894
Nora Khalil <khalil.nor@FACF2XQ43G76D.tld>
2024-08-28 10:55:29 -0400

    Trained Perfluoroalkene_Formation family, new PFAS family.

--- input/kinetics/families/Perfluoroalkene_Formation/rules.py
+++ input/kinetics/families/Perfluoroalkene_Formation/rules.py
@@ -4,4 +4,65 @@
 name = "Perfluoroalkene_Formation/rules"
 shortDesc = ""
 longDesc = """
+
 """
+entry(
+    index = 1,
+    label = "Root",
+    kinetics = ArrheniusBM(A=(1.85013e+13,'s^-1'), n=-0.119091, w0=(786,'kJ/mol'), E0=(368.789,'kJ/mol'), Tmin=(300,'K'), Tmax=(2000,'K'), uncertainty=RateUncertainty(mu=0.6525674998874904, var=380.112630126333, Tref=1000.0, N=3, data_mean=0.0, correlation='Root',), comment="""BM rule fitted to 3 training reactions at node Root
+    Total Standard Deviation in ln(k): 40.72488891792468"""),
+    rank = 11,
+    shortDesc = """BM rule fitted to 3 training reactions at node Root
+Total Standard Deviation in ln(k): 40.72488891792468""",
+    longDesc = 
+"""
+BM rule fitted to 3 training reactions at node Root
+Total Standard Deviation in ln(k): 40.72488891792468
+""",
+)
+
+entry(
+    index = 2,
+    label = "Root_Ext-1C-R",
+    kinetics = ArrheniusBM(A=(6.33333e+09,'s^-1'), n=1.49, w0=(1094.12,'kJ/mol'), E0=(518.256,'kJ/mol'), Tmin=(300,'K'), Tmax=(2000,'K'), uncertainty=RateUncertainty(mu=0.0, var=33.13686319048999, Tref=1000.0, N=1, data_mean=0.0, correlation='Root_Ext-1C-R',), comment="""BM rule fitted to 1 training reactions at node Root_Ext-1C-R
+    Total Standard Deviation in ln(k): 11.540182761524994"""),
+    rank = 11,
+    shortDesc = """BM rule fitted to 1 training reactions at node Root_Ext-1C-R
+Total Standard Deviation in ln(k): 11.540182761524994""",
+    longDesc = 
+"""
+BM rule fitted to 1 training reactions at node Root_Ext-1C-R
+Total Standard Deviation in ln(k): 11.540182761524994
+""",
+)
+
+entry(
+    index = 3,
+    label = "Root_Ext-3C-R_7R!H->C",
+    kinetics = ArrheniusBM(A=(1.23e+06,'s^-1'), n=1.56, w0=(786,'kJ/mol'), E0=(289.945,'kJ/mol'), Tmin=(300,'K'), Tmax=(2000,'K'), uncertainty=RateUncertainty(mu=0.0, var=33.13686319048999, Tref=1000.0, N=1, data_mean=0.0, correlation='Root_Ext-3C-R_7R!H->C',), comment="""BM rule fitted to 1 training reactions at node Root_Ext-3C-R_7R!H->C
+    Total Standard Deviation in ln(k): 11.540182761524994"""),
+    rank = 11,
+    shortDesc = """BM rule fitted to 1 training reactions at node Root_Ext-3C-R_7R!H->C
+Total Standard Deviation in ln(k): 11.540182761524994""",
+    longDesc = 
+"""
+BM rule fitted to 1 training reactions at node Root_Ext-3C-R_7R!H->C
+Total Standard Deviation in ln(k): 11.540182761524994
+""",
+)
+
+entry(
+    index = 4,
+    label = "Root_Ext-3C-R_N-7R!H->C",
+    kinetics = ArrheniusBM(A=(6.23333e+07,'s^-1'), n=1.21, w0=(786,'kJ/mol'), E0=(256.218,'kJ/mol'), Tmin=(300,'K'), Tmax=(2000,'K'), uncertainty=RateUncertainty(mu=0.0, var=33.13686319048999, Tref=1000.0, N=1, data_mean=0.0, correlation='Root_Ext-3C-R_N-7R!H->C',), comment="""BM rule fitted to 1 training reactions at node Root_Ext-3C-R_N-7R!H->C
+    Total Standard Deviation in ln(k): 11.540182761524994"""),
+    rank = 11,
+    shortDesc = """BM rule fitted to 1 training reactions at node Root_Ext-3C-R_N-7R!H->C
+Total Standard Deviation in ln(k): 11.540182761524994""",
+    longDesc = 
+"""
+BM rule fitted to 1 training reactions at node Root_Ext-3C-R_N-7R!H->C
+Total Standard Deviation in ln(k): 11.540182761524994
+""",
+)
+
--- input/kinetics/families/Perfluoroalkene_Formation/training/reactions.py
+++ input/kinetics/families/Perfluoroalkene_Formation/training/reactions.py
@@ -8,7 +8,7 @@ Put kinetic parameters for specific reactions in this file to use as a
 training set for generating rate rules to populate this kinetics family.
 """
 entry(
-    index = 1,
+    index = 0,
     label = "C3F8 <=> CF4 + C2F4",
     degeneracy = 6.0,
     kinetics = Arrhenius(A=(3.8e+10,'s^-1'), n=1.49, Ea=(137246,'cal/mol'), T0=(1,'K')),
@@ -22,7 +22,7 @@ Original entry: C3F8 <=> CF2CF2+CF4
 )
 
 entry(
-    index = 2,
+    index = 1,
     label = "C3F6O <=> CF2O + C2F4",
     degeneracy = 3.0,
     kinetics = Arrhenius(A=(1.87e+08,'s^-1'), n=1.21, Ea=(82079.9,'cal/mol'), T0=(1,'K')),
@@ -36,7 +36,7 @@ Original entry: C2F5CFO <=> CF2CF2+CF2O
 )
 
 entry(
-    index = 3,
+    index = 2,
     label = "C4F8O <=> CF2O + C3F6",
     degeneracy = 2.0,
     kinetics = Arrhenius(A=(2.46e+06,'s^-1'), n=1.56, Ea=(80833,'cal/mol'), T0=(1,'K')),
