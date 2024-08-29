b4b9468704076eef54ca6d05dcf9ba014ecb6ed4
Nora Khalil <khalil.nor@FACF2XQ43G76D.tld>
2024-08-29 08:59:19 -0400

    Retrained Perfluorinated_Alkyl_Rearrangement family, new PFAS family

--- input/kinetics/families/Perfluorinated_Alkyl_Rearrangement/rules.py
+++ input/kinetics/families/Perfluorinated_Alkyl_Rearrangement/rules.py
@@ -4,4 +4,50 @@
 name = "Perfluorinated_Alkyl_Rearrangement/rules"
 shortDesc = ""
 longDesc = """
+
 """
+entry(
+    index = 1,
+    label = "Root",
+    kinetics = ArrheniusBM(A=(184795,'s^-1'), n=1.70472, w0=(1536.5,'kJ/mol'), E0=(367.366,'kJ/mol'), Tmin=(300,'K'), Tmax=(2000,'K'), uncertainty=RateUncertainty(mu=-0.012729637810097694, var=62.28539990888931, Tref=1000.0, N=2, data_mean=0.0, correlation='Root',), comment="""BM rule fitted to 2 training reactions at node Root
+    Total Standard Deviation in ln(k): 15.85357372266047"""),
+    rank = 11,
+    shortDesc = """BM rule fitted to 2 training reactions at node Root
+Total Standard Deviation in ln(k): 15.85357372266047""",
+    longDesc = 
+"""
+BM rule fitted to 2 training reactions at node Root
+Total Standard Deviation in ln(k): 15.85357372266047
+""",
+)
+
+entry(
+    index = 2,
+    label = "Root_Ext-6C-R_Ext-6C-R_Ext-7R!H-R_8R!H->C",
+    kinetics = ArrheniusBM(A=(302000,'s^-1'), n=1.52, w0=(1536.5,'kJ/mol'), E0=(383.856,'kJ/mol'), Tmin=(300,'K'), Tmax=(2000,'K'), uncertainty=RateUncertainty(mu=0.0, var=33.13686319048999, Tref=1000.0, N=1, data_mean=0.0, correlation='Root_Ext-6C-R_Ext-6C-R_Ext-7R!H-R_8R!H->C',), comment="""BM rule fitted to 1 training reactions at node Root_Ext-6C-R_Ext-6C-R_Ext-7R!H-R_8R!H->C
+    Total Standard Deviation in ln(k): 11.540182761524994"""),
+    rank = 11,
+    shortDesc = """BM rule fitted to 1 training reactions at node Root_Ext-6C-R_Ext-6C-R_Ext-7R!H-R_8R!H->C
+Total Standard Deviation in ln(k): 11.540182761524994""",
+    longDesc = 
+"""
+BM rule fitted to 1 training reactions at node Root_Ext-6C-R_Ext-6C-R_Ext-7R!H-R_8R!H->C
+Total Standard Deviation in ln(k): 11.540182761524994
+""",
+)
+
+entry(
+    index = 3,
+    label = "Root_Ext-6C-R_Ext-6C-R_Ext-7R!H-R_N-8R!H->C",
+    kinetics = ArrheniusBM(A=(1.83e+08,'s^-1'), n=0.97, w0=(1536.5,'kJ/mol'), E0=(358.92,'kJ/mol'), Tmin=(300,'K'), Tmax=(2000,'K'), uncertainty=RateUncertainty(mu=0.0, var=33.13686319048999, Tref=1000.0, N=1, data_mean=0.0, correlation='Root_Ext-6C-R_Ext-6C-R_Ext-7R!H-R_N-8R!H->C',), comment="""BM rule fitted to 1 training reactions at node Root_Ext-6C-R_Ext-6C-R_Ext-7R!H-R_N-8R!H->C
+    Total Standard Deviation in ln(k): 11.540182761524994"""),
+    rank = 11,
+    shortDesc = """BM rule fitted to 1 training reactions at node Root_Ext-6C-R_Ext-6C-R_Ext-7R!H-R_N-8R!H->C
+Total Standard Deviation in ln(k): 11.540182761524994""",
+    longDesc = 
+"""
+BM rule fitted to 1 training reactions at node Root_Ext-6C-R_Ext-6C-R_Ext-7R!H-R_N-8R!H->C
+Total Standard Deviation in ln(k): 11.540182761524994
+""",
+)
+
--- input/kinetics/families/Perfluorinated_Alkyl_Rearrangement/training/reactions.py
+++ input/kinetics/families/Perfluorinated_Alkyl_Rearrangement/training/reactions.py
@@ -8,7 +8,7 @@ Put kinetic parameters for specific reactions in this file to use as a
 training set for generating rate rules to populate this kinetics family.
 """
 entry(
-    index = 1,
+    index = 0,
     label = "C4F8O <=> C4F8O-2",
     degeneracy = 1.0,
     kinetics = Arrhenius(A=(1.83e+08,'s^-1'), n=0.97, Ea=(72710,'cal/mol'), T0=(1,'K')),
@@ -22,7 +22,7 @@ Original entry: C2F5OCCF3 <=> CF2OC(CF3)CF3
 )
 
 entry(
-    index = 2,
+    index = 1,
     label = "C5F10O <=> C5F10O-2",
     degeneracy = 1.0,
     kinetics = Arrhenius(A=(302000,'s^-1'), n=1.52, Ea=(74068.3,'cal/mol'), T0=(1,'K')),
